6bdafbde7e8b785300f2f65f78ef165e9571638a
ramiro.sagmit <rogenesagmit@gmail.com>
2023-08-01 13:37:28 +1200

    Initial Commit

---
+++ CardPac/CardPacApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardPacApp.swift
+//  CardPac
+//
+//  Created by rogenesagmit on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CardPacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardPac/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CardPac
+//
+//  Created by rogenesagmit on 8/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
